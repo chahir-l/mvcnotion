4f8302d578c07889c4a650c0d978e33c8a6a2f54
chahir-l <c.loukili@gmail.com>
2020-09-23 11:27:51 +0200

    add dotenv

--- public/index.php
+++ public/index.php
@@ -1,8 +1,15 @@
 <?php
 
 require_once '../vendor/autoload.php';
-require_once '../config/config.php'; 
+require_once '../config/config.php';
 
+
+$dotenv = new Symfony\Component\Dotenv\Dotenv();
+$dotenv->load(__DIR__.'../.env');
+
+$dbUser = getenv('DB_USER');
+$dbUser = $_ENV['DB_USER'];
+$dbUser = $_SERVER['DB_USER'];
  
 
 use Core\App;
